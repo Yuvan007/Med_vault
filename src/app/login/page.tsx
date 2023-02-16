4134cc50c75463c99f9706075af1c7e711434f8d
niceguynix <its200manoj@gmail.com>
2023-02-17 00:38:49 +0530

    del:login/page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,8 @@
-'use client'
-
-import { useState } from "react";
 
 function Page(){
 
-    const [count,setCount]=useState(0);
-
     return (
         <>
-           <button onClick={()=>setCount(count+1)}>+</button> 
-           {count}
         </>
     )
 }
