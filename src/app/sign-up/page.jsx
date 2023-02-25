24ea7b6a281a6a8c5e26243f4bd44f63ad1c1f3d
VishaalC <vishaalc1@gmail.com>
2023-02-25 14:38:50 +0530

    Add styles

---
+++ src/app/sign-up/page.jsx
@@ -0,0 +1,27 @@
+export default function Page() {
+    return (
+        <div className="container">
+            <div className="left-section">
+                <h2>MedVault</h2>
+                <h4>Start digitizing your health records with us</h4>
+            </div>
+            <div className="right-section">
+                <div className="header">
+                    <h1>Already a member? Sign in</h1>
+                </div>
+                <h1>Sign up</h1>
+                <div className="third-p">
+                    <button>Google</button>
+                    <button>Twitter</button>
+                </div>
+                <div className="sign-form">
+                    <form>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
