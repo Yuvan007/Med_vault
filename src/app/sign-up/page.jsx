850813a83a6c371c69bfcd65cae9884cc64d3b2c
VishaalC <vishaalc1@gmail.com>
2023-02-25 19:35:40 +0530

    Finish layout

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -17,8 +17,8 @@ export default function Page() {
                 <div className="header">
                     <h1>Already a member? <Link style={{ color: "blue" }} href="/login">Sign in</Link></h1>
                 </div>
-                <h1 className="Sign font-bold text-3xl">Sign up to MedVault</h1>
-                <div className="third-p mt-5 mb-8">
+                <h1 className="Sign mb-10 font-bold text-3xl">Sign up to MedVault</h1>
+                <div className="third-p mt-5 mb-20">
                     <Link href="#" className="mt-5 flex justify-evenly p-1 text-center border-2 border-slate-500 rounded-md font-bold"><Image src={GoogleSVG} className="my-auto w-5 h-5" alt="Google" /><span className="mr-2 ml-2">Google</span></Link>
                     <Link href="#" className="mt-5 flex justify-evenly p-1 text-center border-2 border-slate-500 rounded-md font-bold"><Image src={FacebookSVG} className="my-auto w-5 h-5" alt="Facebook" /><span className="mr-2 ml-2">Facebook</span></Link>
                 </div>
@@ -27,27 +27,30 @@ export default function Page() {
                     <h2>Or</h2>
                     <div className="line"></div>
                 </div>
-                <div className="sign-form">
-                    <form action="#" method="GET">
-                        <div className="first">
-                            <label htmlFor="name">Name: </label>
-                            <input type="text" name="name" required={true} />
-                            <label htmlFor="usern">Username: </label>
-                            <input type="text" name="usern" required={true} />
+                <div className="sign-up-form">
+                    <form>
+                        <div className="form font-bold text-2xl">
+                            <label htmlFor="fname">First name: </label><br></br>
+                            <input className="h-10 p-1 my-1 border-2 rounded-md border-slate-500" type="text" id="fname" name="fname"></input>
                         </div>
-                        <div>
-                            <label htmlFor="pass">Password: </label>
-                            <input type="password" name="pass" required={true} />
+                        <div className="form font-bold text-2xl">
+                            <label htmlFor="usern">Username: </label><br></br>
+                            <input className="h-10 p-1 my-1 border-2 rounded-md border-slate-500" type="text" id="usern" name="usern"></input>
                         </div>
-                        <div>
-                            <label htmlFor="email">Email: </label>
-                            <input type="email" name="email" required={true} />
+                        <div className="form font-bold text-2xl">
+                            <label htmlFor="email">Email: </label><br></br>
+                            <input className="h-10 p-1 my-1 border-2 rounded-md border-slate-500" type="email" id="email" name="email"></input>
                         </div>
-                        <button>Submit</button>
+                        <div className="form font-bold text-2xl">
+                            <label htmlFor="passw">Password: </label><br></br>
+                            <input className="h-10 p-1 my-1 border-2 rounded-md border-slate-500" type="password" id="passw" name="passw"></input>
+                        </div>
+                        <div className="button">
+                            <button className="py-2 px-3 rounded-lg font-[500] bg-[rgba(255,202,132,255)]">Create Account</button>
+                        </div>
                     </form>
-
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
